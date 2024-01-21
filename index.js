9843b248d9345a2ce23364a807d074b7b9806854
Riston <ristonthomas@gmail.com>
2024-01-21 11:58:42 +0400

    more images for background
    updated the images array to have more images

--- index.js
+++ index.js
@@ -50,14 +50,23 @@ function changeBackground() {
     // Array of image URLs
     const images = [
         'url("https://storage.googleapis.com/webdesignledger.pub.network/LaT/edd/2016/02/tree-bark-nature-texture-6.jpg")',
-        'url("https://img.freepik.com/free-photo/beige-dried-leaf-pattern-wallpaper_53876-94651.jpg")',
-        'url("https://lh3.googleusercontent.com/proxy/IohxVEBKU9rkPubwnDxhLdJV0HmUbZWJ0w52mrnv5fYWpsW_F_wnYlWYoZa6VaPFmGHr8WQklatXFUHlCBWPwnTO0mYhXJi3QOAjutH_ZMv1Fw")',
+        'url("https://i.pinimg.com/736x/a5/90/51/a590519c41cc36261542d5274c7cd948.jpg")',
+        'url("https://images.saatchiart.com/saatchi/1067587/art/7421899/6491101-HSC00002-7.jpg")',
         'url("https://i.pinimg.com/originals/b5/df/2a/b5df2adcd978bf0ff7aa6f4b8a7b9f52.jpg")',
         'url("https://g3.img-dpreview.com/B7B8A5ABFA9D4A8A85F15EE817F11862.jpg")',
         'url("https://img.freepik.com/free-photo/green-leaf-texture-leaf-texture-background_501050-120.jpg?size=626&ext=jpg&ga=GA1.1.1880011253.1700524800&semt=ais")',
         'url("https://img.freepik.com/premium-photo/tropical-leaves-abstract-green-leaves-texture-nature-background_512343-1135.jpg")',
         'url("https://img.freepik.com/free-vector/large-tropical-green-leaves-background_52683-36490.jpg?size=626&ext=jpg&ga=GA1.1.632798143.1705363200&semt=ais")',
-    ];
+        'url("https://as2.ftcdn.net/v2/jpg/03/59/83/71/1000_F_359837180_Aj7LJTQlMl6wN7r7euXwqwvzMve1gSHg.jpg")',
+        'url("https://dspncdn.com/a1/media/692x/50/13/38/501338a1534f099a48b5f178bd92c436.jpg")',
+        'url("https://wallup.net/wp-content/uploads/2016/02/18/258259-nature-trees-forest-leaves-lianas-mist-moss-path-plants-ferns-rainforest-jungles-748x468.jpg")',
+        'url("https://c1.wallpaperflare.com/preview/852/431/694/green-fern-leaves-texture-nature-forest.jpg")',
+        'url("https://www.shutterstock.com/image-photo/green-leaves-flat-lay-nature-260nw-2301361577.jpg")',
+        'url("https://i.pinimg.com/736x/c9/57/19/c95719fd8c80b74c1d0e2fd7c12d6130.jpg")',
+        'url("https://www.qpanel.eu/wp-content/uploads/QD10.01.003.jpg")',
+        'url("https://img.freepik.com/premium-photo/natural-fern-leaves_97245-89.jpg?size=626&ext=jpg")',
+    ];  
+    
     const randomIndex = Math.floor(Math.random() * images.length);
     let calcElement = document.querySelector(".calc");
     calcElement.style.backgroundImage = images[randomIndex];
