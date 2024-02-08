b5bd06d25dbfd62023f00a694e2d100e806d0660
Riston <ristonthomas@gmail.com>
2024-02-08 07:44:55 +0400

    v 0.1
    working model

--- index.js
+++ index.js
@@ -1,16 +1,69 @@
 console.log("js is loaded");
-let num1 = 8
-let num2 = 2
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
+let num1 = Math.floor(Math.random() * 20) + 1;
+let num2 = Math.floor(Math.random() * 20) + 1;
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
+let ans1 = num1 + num2;
+let ans2 = ans1 + Math.floor(Math.random() * 5);
+let ans3 = ans1 - Math.floor(Math.random() * 5);
+let ans4 = Math.floor(Math.random() * 20);
+assign_cards();
 
-// Create four functions: add(), subtract(), divide(), multiply()
-// Call the correct function when the user clicks on one of the buttons
-// Perform the given calculation using num1 and num2
-// Render the result of the calculation in the paragraph with id="sum-el"
+function assign_cards(){
+    if (ans1 === ans2) {
+        ans2 = ans1 + Math.floor(Math.random() * 5 + 1) ;
+    }
+    if (ans1 === ans3) {
+        ans3 = ans1 - Math.floor(Math.random() * 5 + 1);
+    }
+    
+    if (ans1 === ans4) {
+        ans4 = Math.floor(Math.random() * 20);
+    }
+    
+    if (ans2 === ans3 || ans3 === ans4) {
+        ans3 = ans1 - Math.floor(Math.random() * 5 + 1);
+        if (ans1 === ans3) {
+            ans3 = ans1 - Math.floor(Math.random() * 5 + 1);
+        }
+    }
+    
+    if (ans3 === ans4) {
+        ans4 = Math.floor(Math.random() * 20 + 1);
+        if (ans1 === ans4) {
+            ans4 = Math.floor(Math.random() * 20 + 1);
+        }
+    }
+    
+    let k = Math.floor(Math.random()*4 + 1);
+    
+    if (k === 1) {
+        document.getElementById("ans1").textContent = ans1;
+        document.getElementById("ans2").textContent = ans2;
+        document.getElementById("ans3").textContent = ans3;
+        document.getElementById("ans4").textContent = ans4;
+    } else if (k === 2) {
+        document.getElementById("ans1").textContent = ans2;
+        document.getElementById("ans2").textContent = ans1;
+        document.getElementById("ans3").textContent = ans3;
+        document.getElementById("ans4").textContent = ans4;
+    } else if (k === 3) {
+        document.getElementById("ans1").textContent = ans2;
+        document.getElementById("ans2").textContent = ans3;
+        document.getElementById("ans3").textContent = ans1;
+        document.getElementById("ans4").textContent = ans4;
+    } else {
+        document.getElementById("ans1").textContent = ans2;
+        document.getElementById("ans2").textContent = ans3;
+        document.getElementById("ans3").textContent = ans4;
+        document.getElementById("ans4").textContent = ans1; 
+    }
+}
 
-// E.g. if the user clicks on the "Plus" button, you should render
-// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+
+
+
+
 
 function add() {
     let sum = num1 + num2;
@@ -41,33 +94,9 @@ function next() {
     num2 = Math.floor(Math.random() * 20) + 1; // Random integer between 1 and 20
     document.getElementById("num1-el").textContent = num1
     document.getElementById("num2-el").textContent = num2
-    let ans = document.getElementById("sum-el");
-    ans.innerText = "Sum:";
-    changeBackground();
-}
-
-function changeBackground() {
-    // Array of image URLs
-    const images = [
-        'url("https://storage.googleapis.com/webdesignledger.pub.network/LaT/edd/2016/02/tree-bark-nature-texture-6.jpg")',
-        'url("https://i.pinimg.com/736x/a5/90/51/a590519c41cc36261542d5274c7cd948.jpg")',
-        'url("https://images.saatchiart.com/saatchi/1067587/art/7421899/6491101-HSC00002-7.jpg")',
-        'url("https://i.pinimg.com/originals/b5/df/2a/b5df2adcd978bf0ff7aa6f4b8a7b9f52.jpg")',
-        'url("https://g3.img-dpreview.com/B7B8A5ABFA9D4A8A85F15EE817F11862.jpg")',
-        'url("https://img.freepik.com/free-photo/green-leaf-texture-leaf-texture-background_501050-120.jpg?size=626&ext=jpg&ga=GA1.1.1880011253.1700524800&semt=ais")',
-        'url("https://img.freepik.com/premium-photo/tropical-leaves-abstract-green-leaves-texture-nature-background_512343-1135.jpg")',
-        'url("https://img.freepik.com/free-vector/large-tropical-green-leaves-background_52683-36490.jpg?size=626&ext=jpg&ga=GA1.1.632798143.1705363200&semt=ais")',
-        'url("https://as2.ftcdn.net/v2/jpg/03/59/83/71/1000_F_359837180_Aj7LJTQlMl6wN7r7euXwqwvzMve1gSHg.jpg")',
-        'url("https://dspncdn.com/a1/media/692x/50/13/38/501338a1534f099a48b5f178bd92c436.jpg")',
-        'url("https://wallup.net/wp-content/uploads/2016/02/18/258259-nature-trees-forest-leaves-lianas-mist-moss-path-plants-ferns-rainforest-jungles-748x468.jpg")',
-        'url("https://c1.wallpaperflare.com/preview/852/431/694/green-fern-leaves-texture-nature-forest.jpg")',
-        'url("https://www.shutterstock.com/image-photo/green-leaves-flat-lay-nature-260nw-2301361577.jpg")',
-        'url("https://i.pinimg.com/736x/c9/57/19/c95719fd8c80b74c1d0e2fd7c12d6130.jpg")',
-        'url("https://www.qpanel.eu/wp-content/uploads/QD10.01.003.jpg")',
-        'url("https://img.freepik.com/premium-photo/natural-fern-leaves_97245-89.jpg?size=626&ext=jpg")',
-    ];  
-    
-    const randomIndex = Math.floor(Math.random() * images.length);
-    let calcElement = document.querySelector(".calc");
-    calcElement.style.backgroundImage = images[randomIndex];
+    ans1 = num1 + num2;
+    ans2 = ans1 + Math.floor(Math.random() * 5);
+    ans3 = ans1 - Math.floor(Math.random() * 5);
+    ans4 = Math.floor(Math.random() * 20) + 1;
+    assign_cards();
 }
